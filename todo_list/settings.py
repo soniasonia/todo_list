c1363c70dc2c2d585fd626bb37c5023dd5efccd4
Sonia P <pawlaksonia@gmail.com>
2019-08-17 22:58:42 +0200

    Adapted project to Heroku

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -120,3 +120,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# djang-heroku setting
+import django_heroku
+
+django_heroku.settings(locals())
+
+# static files settings
+# location where you will store your static files like bootstrap
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
+# location where django collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
